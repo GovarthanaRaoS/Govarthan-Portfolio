942c5f9c798a0b1449d19085b64bed8f6e45e570
GovarthanaRaoS <govarthanarao123@gmail.com>
2023-09-04 02:14:27 +0530

    remove few console log

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -14,61 +14,61 @@ export class AboutMeComponent {
   handleNext(){
     if(this.work_card==2){
       this.work_card = 0;
-      console.log(this.work_card);
+      // console.log(this.work_card);
       return;
     }
     this.work_card = this.work_card+1;
-    console.log(this.work_card);
+    // console.log(this.work_card);
   }
 
   handlePrevious(){
     if(this.work_card==0){
       this.work_card = 2;
-      console.log(this.work_card);
+      // console.log(this.work_card);
       return;
     }
     this.work_card = this.work_card-1;
-    console.log(this.work_card);
+    // console.log(this.work_card);
   }
 
   handleSkillNext(){
     if(this.skill_card==1){
       this.skill_card = 0;
-      console.log(this.skill_card);
+      // console.log(this.skill_card);
       return;
     }
     this.skill_card = this.skill_card+1;
-    console.log(this.work_card);
+    // console.log(this.work_card);
   }
 
   handleSkillPrev(){
     if(this.skill_card==0){
       this.skill_card = 1;
-      console.log(this.skill_card);
+      // console.log(this.skill_card);
       return;
     }
     this.skill_card = this.skill_card-1;
-    console.log(this.skill_card);
+    // console.log(this.skill_card);
   }
 
   handleEduPrev(){
     if(this.edu_card==0){
       this.edu_card = 1;
-      console.log(this.edu_card);
+      // console.log(this.edu_card);
       return;
     }
     this.edu_card = this.edu_card-1;
-    console.log(this.edu_card);
+    // console.log(this.edu_card);
   }
 
   handleEduNext(){
     if(this.edu_card==1){
       this.edu_card = 0;
-      console.log(this.edu_card);
+      // console.log(this.edu_card);
       return;
     }
     this.edu_card = this.edu_card+1;
-    console.log(this.edu_card);
+    // console.log(this.edu_card);
   }
 
 }
